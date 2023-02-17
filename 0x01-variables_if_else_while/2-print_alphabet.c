79cad6f175ed936a549790bceec7e564e4c22de0
georgeAkonjom <georgeakonjom003@gmail.com>
2023-02-17 13:32:16 -0800

    print alpha 0

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - This is a description
+ *
+ * Return: Description of the returned value
+ */
+
+int main(void){
+	int x;
+
+	for(x = 'a'; x <= 'z'; x++)
+{
+		putchar(x);
+}
+	return (0);
+}
