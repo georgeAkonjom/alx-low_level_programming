6e7ec0b0d18ea1bd0249f57d3e1d8277ebafeb4a
georgeAkonjom <georgeakonjom003@gmail.com>
2023-02-17 15:42:10 -0800

    numbers 0, numberz 0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - This is a description
+ *
+ * Return: Description of the returned value
+ */
+
+int main(void)
+{
+	for (int i = 0; i <= 9; i++)
+	{
+		putchar('0' + i);
+	}
+	putchar('\n');
+	return (0);
+}
