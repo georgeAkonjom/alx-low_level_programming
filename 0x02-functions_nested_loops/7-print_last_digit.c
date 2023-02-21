a7a59927b056b37e2afdaa1f552b707a0d911163
georgeAkonjom <georgeakonjom003@gmail.com>
2023-02-21 15:43:28 -0800

    new commit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isalpha - identifys alphabetical chars
+ *
+ * @c: description here 
+ *
+ * Return: Always 0.
+ */
+
+int print_last_digit(int c)
+{
+	c = c % 10;
+	return (c);
+}
