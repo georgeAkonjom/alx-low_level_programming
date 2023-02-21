1e1486f46059c5d5d56807e74b8b513d6cf083fc
georgeAkonjom <georgeakonjom003@gmail.com>
2023-02-21 07:18:00 -0800

    alphabet 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+        int x;
+
+	for (x = 'a'; x <= 'z'; x++)
+		_putchar(x);
+        _putchar('\n');
+	return (0);
+}
