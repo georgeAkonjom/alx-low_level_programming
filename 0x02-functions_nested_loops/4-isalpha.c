b9fd6372e0e20df25e1efdd7116ebe7a06aa4fdf
georgeAkonjom <georgeakonjom003@gmail.com>
2023-02-21 13:55:40 -0800

    new commit

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha - identifys alphabetical chars
+ *
+ * @c: description here 
+ *
+ * Return: Always 0.
+ */
+
+int _isalpha(int c);
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	else if (c >= 65 && c <= 90)
+		return (1);
+	return (0);
+
+}
