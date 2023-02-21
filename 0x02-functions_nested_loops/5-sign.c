328752da735b328fff793ce11c626d0212f46f9d
georgeAkonjom <georgeakonjom003@gmail.com>
2023-02-21 14:19:54 -0800

    new commit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,15 +13,15 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		return (1);
-		putchar("+");
+		_putchar("+");
 	}
 	else if (n < 0)
 	{
 		return (-1);
-		putchar("-");
+		_putchar("-");
 	}else if (n == 0)
 	{
 		return (0);
-		putchar("0")
+		_putchar("0")
 	}
 }
