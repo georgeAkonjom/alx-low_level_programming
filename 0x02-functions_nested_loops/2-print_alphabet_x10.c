3ab6776f8e291276c8e817a1aebc29988907a195
georgeAkonjom <georgeakonjom003@gmail.com>
2023-02-21 11:15:10 -0800

    a

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,13 +11,13 @@ void print_alphabet_x10(void)
 {
 	int i = 0;
 
-while (i <= 10)
+	while (i <= 10)
 	{
 		char x = 'a';
 
 		while (x <= 'z')
 		{
-			_putchar("%c", x);
+			_putchar(x);
 			i++;
 		}
 	}
