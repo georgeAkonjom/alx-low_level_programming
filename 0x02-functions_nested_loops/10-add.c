87a55de2474b7ada7aec4c57e3e5f2dcb48019ec
georgeAkonjom <georgeakonjom003@gmail.com>
2023-02-21 16:31:03 -0800

    new commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - identifys alphabetical chars
+ *
+ * @c: description here 
+ *
+ * Return: Always 0.
+ */
+
+int add(int x, int y)
+{
+	int result = x + y;
+
+	return (result);
+}
