81a9c4066fee2910ebd7f4dd4f43547ffc9e14c7
georgeAkonjom <georgeakonjom003@gmail.com>
2023-02-23 15:01:43 -0800

    numbers 1, multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - identifys alphabetical chars
+ *
+ * @a: int
+ * @b: int
+ *
+ * Return: Always 0.
+ */
+
+int add(int a, int b)
+{
+	int result = x * y;
+
+	return (result);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * add - identifys alphabetical chars
+ *
+ * @x: int
+ * @y: int
+ *
+ * Return: Always 0.
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i > 9; i++)
+		_putchar(i);
+	_putchar('\n')
+	return (0);
+}
