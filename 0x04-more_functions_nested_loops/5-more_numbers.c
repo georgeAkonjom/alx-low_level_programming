e3ecc5c8e2a45c2dad52b3154e64ece1598115fc
georgeAkonjom <georgeakonjom003@gmail.com>
2023-02-23 15:43:31 -0800

    task update

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * more_numbers - identifys alphabetical chars
+ * Return: Always 0.
+ */
+
+void more_numbers(void)
+{
+	int c;
+	int i;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (c = 0; c <= 14; c++)
+			_putchar(c + 48);
+		_putchar('\n')
+	}
+	_putchar('\n');
+}
