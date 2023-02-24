f1edd4920f285ebbde9ef1fe0e7a55340ceea51e
georgeAkonjom <georgeakonjom003@gmail.com>
2023-02-24 05:23:37 -0800

    a commit here

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - check the code
+ *
+ * Return: Always 0.
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i <= n; i++)
+			_putchar('_');
+	}
+	else 
+		_putchar('\n');
+}
