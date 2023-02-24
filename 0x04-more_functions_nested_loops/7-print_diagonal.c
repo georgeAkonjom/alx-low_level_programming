9f4c7ae20939436d631c5ac75d8f9f8263cf634f
georgeAkonjom <georgeakonjom003@gmail.com>
2023-02-24 08:25:12 -0800

    task update

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - check the code
+ *
+ * Return: Always 0.
+ */
+void print_line(int n)
+{
+	int lines;
+	int spaces;
+
+	if (n > 0)
+	{
+		for (lines = 0; lines <= n; lines++)
+		{
+			_putchar(92);
+			_putchar('\n')
+			for (spaces = 1; spaces <= lines; spaces++)
+				_putchar(' ')
+
+		}
+	}
+	else 
+		_putchar('\n');
