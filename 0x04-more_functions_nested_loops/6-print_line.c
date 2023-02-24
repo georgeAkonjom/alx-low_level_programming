73aac6fd72878b7239ead20a3e5101d293ec7414
georgeAkonjom <georgeakonjom003@gmail.com>
2023-02-23 18:01:18 -0800

    task update

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * print_line - check the code
  *
  * Return: Always 0.
  */
-void print(int n)
+void print_line(int n)
 {
 	if (n > 0)
 		_putchar("_");
