3bbe807fed0cce8d297530ade85cfcdd34661038
georgeAkonjom <georgeakonjom003@gmail.com>
2023-03-06 08:48:23 -0800

    forgot the readme file.

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,7 +1,7 @@
 #ifndef __FILE_NAME_H__
 #define __FILE_NAME_H__
 
-#include <Stdio.h>
+#include <stdio.h>
 
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
