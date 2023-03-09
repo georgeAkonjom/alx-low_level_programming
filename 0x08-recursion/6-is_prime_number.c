8ec083d0ea497d48646fc15a481a714aa90eb871
georgeAkonjom <georgeakonjom003@gmail.com>
2023-03-09 13:46:18 -0800

    task update

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -10,12 +10,13 @@ int is_prime_number(int n)
 {
 	int i = 2;
 
-	if (n < 2)
+	if (n <= 1)
+		return (0);
+
+	if (n % i == 0 && n != i)
 		return (0);
 	if (n == i)
 		return (1);
-	if (n % i == 0)
-		return (0);
 	i++;
 	return is_prime_number(n);
 }
