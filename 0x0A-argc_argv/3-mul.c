edccb1ae84d5ac5c03c4c72f5c61e88dae6978b1
georgeAkonjom <georgeakonjom003@gmail.com>
2023-03-12 16:47:57 -0700

    task update

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiples the content of argv
+ *
+ * @argv: should contain two integers
+ * @argc: should print two
+ * Return: 0 success 1 error
+ */
+int main(int argc, char *argv[])
+{
+	int mul;
+	
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	mul = (atoi(argv[1])) * (atoi(argv[2]));
+	printf("%d\n", mul);
+
+}
