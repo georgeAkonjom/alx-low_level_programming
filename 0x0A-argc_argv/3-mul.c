31c38f7819c1ba85ab13198bf685f9e792e5029f
georgeAkonjom <georgeakonjom003@gmail.com>
2023-03-12 16:50:43 -0700

    bug fixes

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -16,7 +16,10 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-	mul = (atoi(argv[1])) * (atoi(argv[2]));
-	printf("%d\n", mul);
+	else
+	{
+		mul = (atoi(argv[1])) * (atoi(argv[2]));
+		printf("%d\n", mul);
+	}
 
 }
