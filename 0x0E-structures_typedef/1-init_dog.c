44947416d5d9e44c11c78928e8066a6d647f27ec
georgeAkonjom <georgeakonjom003@gmail.com>
2023-03-21 15:55:24 -0700

    big fixes

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,13 @@
 #include "dog.h"
-
+/**
+ * init_dog - init a struct of type dog.
+ * @d: pointer to a struct. theres pointers to everything apparently
+ * @name: struct name element.
+ * @age: age element. dogs should live longer
+ * @owner: owners name element.
+ *
+ * Return: void
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d)
