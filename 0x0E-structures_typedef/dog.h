f8440990fd15075e3e5819277b0934ecb9dc017d
georgeAkonjom <georgeakonjom003@gmail.com>
2023-03-21 15:20:36 -0700

    bug fixes

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef __DOG_H__
+#define __DOG_H__
+/**
+ * struct dog - a struct of type dog.
+ * @name: dogs name
+ * @age: dogs age
+ * @owner: owners name
+ *
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+}
+
+#endif /* __DOG_H__ */
+
