46368f0b5183dbdbc67a8e0817755f3ba0ced6b2
georgeAkonjom <georgeakonjom003@gmail.com>
2023-03-20 21:30:24 -0700

    task update

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,1 +1,6 @@
+#ifndef __FILE_NAME_H__
+#define __FILE_NAME_H__
+
 #define SIZE 1024
+
+#endif /* __FILE_NAME_H__ */
--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,1 +1,6 @@
+#ifndef __FILE_NAME_H__
+#define __FILE_NAME_H__
+
 #define PI 3.14159265359
+
+#endif /* __FILE_NAME_H__ */
