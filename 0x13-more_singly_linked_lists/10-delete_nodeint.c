5c1c84753656027854c305904888ebed36f838d1
george akonjom <georgeakonjom003@gmail.com>
2023-04-05 01:23:56 +0000

    task update

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -15,22 +15,25 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	if (*head == NULL)
 		return (-1);
+
 	if (index == 0)
 	{
 		*head = (*head)->next;
 		free(current);
 		return (1);
 	}
+
 	if (current->next)
 	{
 		for (i = 0; i < index - 1 && current->next != NULL; i++)
 		{
 			current = current->next;
 		}
-
-		buffer = current->next;
-		current->next = buffer->next;
-		free(buffer);
 	}
+
+	buffer = current->next;
+	current->next = buffer->next;
+	free(buffer);
+
 	return (1);
 }
