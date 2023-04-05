4baac5f3de924c203c844c333dabe69b7c7d674d
george akonjom <georgeakonjom003@gmail.com>
2023-04-05 01:15:58 +0000

    task update

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * delete_node_int_at_index - deletes the node referred to by index.
+ * delete_nodeint_at_index - deletes the node referred to by index.
  * @head: a double pointer to a struct. i think.
  * @index: an int. the refference to the node to be deleted.
  * Return: 1 if success -1 if fail.
@@ -15,7 +15,6 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	if (*head == NULL)
 		return (-1);
-	
 	if (index == 0)
 	{
 		*head = (*head)->next;
@@ -31,7 +30,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 		buffer = current->next;
 		current->next = buffer->next;
-		free(buffer);	
+		free(buffer);
 	}
 	return (1);
 }
