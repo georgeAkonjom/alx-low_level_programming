a8c7cba93e995b9f799916f99add6280ce414c70
george akonjom <georgeakonjom003@gmail.com>
2023-04-10 00:43:14 +0000

    task update

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- *
- *
- *
+ * flip_bits - how many bits need to change for one no to become the other.
+ * @n: one no.
+ * @m: the other.
+ * Return: number of flips.
  */
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
