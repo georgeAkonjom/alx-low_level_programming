bd5fe41c4d5b13d718a3bd4dcc98e5548c676384
samuel-george <georgeakonjom003@gmail.com>
2023-05-12 17:36:27 +0100

    message here

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,5 +10,8 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd;
 
-	fd = open(filename, O_CREAT);
+	fd = open(filename, O_RDWR | O_CREAT);
+	if (fd == -1)
+		return (-1);
+	write_ret = write(filename, text_content, )
 }
